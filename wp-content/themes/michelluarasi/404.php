2f5dd46bc4422d97d5f11de88ad89b1bb3110d46
Michel Luarasi <michel@luarasi.com>
2017-02-12 16:53:55 +0100

    Minor updates
    Replaced “one-column” class

--- wp-content/themes/michelluarasi/404.php
+++ wp-content/themes/michelluarasi/404.php
@@ -10,7 +10,7 @@ $current_page = "404";
 
 get_header(); ?>
 
-<div class="one-column page-not-found">
+<div class="content-640 page-not-found">
 	<img src="<?php bloginfo('template_url'); ?>/img/fox-404.gif" alt="Not Found"/>
 	<h2>Page Not Found</h2>
 	<p>You're on the wrong path.</p>
