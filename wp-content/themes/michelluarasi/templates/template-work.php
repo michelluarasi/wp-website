cb5fc2f75b364654072520c31b480d8ef60eecd5
Michel Luarasi <michel@luarasi.com>
2017-02-12 21:24:22 +0100

    Title Fix
    Fixed title seo problem

--- wp-content/themes/michelluarasi/templates/template-work.php
+++ wp-content/themes/michelluarasi/templates/template-work.php
@@ -25,8 +25,8 @@ get_header();?>
     while ( have_posts() ) : the_post();
     setup_postdata( $post );
 
-    $title = simple_fields_get_post_value(get_the_id(), "Title", true);
-    $subtitle = simple_fields_get_post_value(get_the_id(), "Subtitle", true);
+    $post_title = simple_fields_get_post_value(get_the_id(), "Post Title", true);
+    $post_subtitle = simple_fields_get_post_value(get_the_id(), "Post Subtitle", true);
 
     $thumbnail_image = simple_fields_get_post_value(get_the_id(), "Teaser Image", true);
     $thumbnail_image_url = wp_get_attachment_url($thumbnail_image);
@@ -35,8 +35,8 @@ get_header();?>
     <li class="work-list__item js-vp_reveal js-slide_up">
       <a class="work-list__item__link" href="<?php echo get_permalink()?>">
         <img class="work-list__item__img" alt="<?php echo $title ?> Teaser Image" src="<?php echo $thumbnail_image_url; ?>" />
-        <h2 class="work-list__item__info__header"><?php echo $title; ?></h2>
-        <p class="work-list__item__info__subtitle"><?php echo $subtitle; ?></p> 
+        <h2 class="work-list__item__info__header"><?php echo $post_title; ?></h2>
+        <p class="work-list__item__info__subtitle"><?php echo $post_subtitle; ?></p> 
       </a>
     </li>
   <?php           
